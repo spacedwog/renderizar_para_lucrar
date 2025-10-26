3fdc004cd01f163a2249d863673c384ce60e55ce
Renderizar Para Lucrar Dev <dev@renderizarparalucrar.com>
2025-10-26 12:42:37 -0300

    Commited

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.assetExts.push('db', 'mp3', 'ttf', 'obj');
-
 module.exports = config;
